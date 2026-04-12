10e125162fa20e29324fada89096d2be4947eee0
Piscki F. Pratama <pisckipratama@gmail.com>
2026-04-12 17:33:53 +0700

    add python lab

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        "time": datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        "hostname": socket.gethostname()
+    }), 200
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({"status":"UP"}), 200
+
+if __name__ == '__main__':
+    app.run()
