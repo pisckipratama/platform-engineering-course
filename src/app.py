2bc8d4ffe0ec15affce9b32144a0cd2bba7afcb4
Piscki F. Pratama <pisckipratama@gmail.com>
2026-04-14 15:46:44 +0700

    edit file app.py and create github action workflow

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         "time": datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
-        "hostname": socket.gethostname()
+        "hostname": socket.gethostname(),
+        "message": "You're doing greate, Human!"
     }), 200
 
 @app.route('/api/v1/healthz')
